f17b136fb4ca6bbd83f86e6319b2e8497c0e543e
sancg <sanblackx1@gmail.com>
2020-07-22 04:10:13 -0500

    Clone Instagram base

--- pygram/urls.py
+++ pygram/urls.py
@@ -1,3 +1,4 @@
+# cSpell: Disable
 """pygram URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -18,6 +19,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [path("admin/", admin.site.urls),] + static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-)
+from posts.views import PostList, PostCreate, PostDetail
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", PostList.as_view(), name="List"),
+    path("post/", PostCreate.as_view(), name="post"),
+    path("post/<pk>/", PostDetail.as_view(), name="detail"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
